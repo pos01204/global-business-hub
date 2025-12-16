e78f7a8778f6cf67687b151c3474509b15a947a6
pos01204 <jihoon@backpac.kr>
2025-12-16 13:38:27 +0900

    오류 수정

--- frontend/components/business-brain/index.ts
+++ frontend/components/business-brain/index.ts
@@ -9,32 +9,32 @@
 export * from './charts'
 
 // 분석 컴포넌트
-export { default as AnalysisDetailDrawer } from './AnalysisDetailDrawer'
-export { default as ChartDrillDownModal } from './ChartDrillDownModal'
+export { AnalysisDetailDrawer } from './AnalysisDetailDrawer'
+export { ChartDrillDownModal } from './ChartDrillDownModal'
 
 // 신뢰도 및 데이터 품질
-export { default as ConfidenceBadge } from './ConfidenceBadge'
-export { default as ConfidenceInterval } from './ConfidenceInterval'
-export { default as DataQualityIndicator } from './DataQualityIndicator'
+export { ConfidenceBadge } from './ConfidenceBadge'
+export { ConfidenceInterval } from './ConfidenceInterval'
+export { DataQualityIndicator } from './DataQualityIndicator'
 
 // 고급 분석 대시보드
-export { default as CLVDashboard } from './CLVDashboard'
-export { default as ChurnRiskList } from './ChurnRiskList'
-export { default as ForecastPerformance } from './ForecastPerformance'
-export { default as SimulationResults } from './SimulationResults'
+export { CLVDashboard } from './CLVDashboard'
+export { ChurnRiskList } from './ChurnRiskList'
+export { ForecastPerformance } from './ForecastPerformance'
+export { SimulationResults } from './SimulationResults'
 
 // 리포트 생성
-export { default as ReportGenerator } from './ReportGenerator'
+export { ReportGenerator } from './ReportGenerator'
 
 // UX 혁신 뷰 (v5.1, v5.2)
-export { default as CommandCenter } from './CommandCenter'
-export { default as ActionHub } from './ActionHub'
-export { default as DeepDive } from './DeepDive'
-export { default as Navigation, NavigationTabs } from './Navigation'
+export { CommandCenter } from './CommandCenter'
+export { ActionHub } from './ActionHub'
+export { DeepDive } from './DeepDive'
+export { Navigation, NavigationTabs } from './Navigation'
 
 // 위젯
-export { default as BrainWidget } from './BrainWidget'
+export { BrainWidget } from './BrainWidget'
 
 // 에러 처리
-export { default as ErrorBoundary } from './ErrorBoundary'
+export { ErrorBoundary } from './ErrorBoundary'
 
