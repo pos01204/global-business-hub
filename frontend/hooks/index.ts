eddaa6b78ad6abaa930e8a141f0b4eee61017c06
pos01204 <jihoon@backpac.kr>
2025-12-16 14:56:32 +0900

    오류 수정

--- frontend/hooks/index.ts
+++ frontend/hooks/index.ts
@@ -1,3 +1,10 @@
 // Hooks - Global Business Hub
 
 export { useMediaQuery, useIsMobile, useIsTablet, useIsDesktop } from './useMediaQuery'
+export { 
+  useKeyboardShortcuts, 
+  useShortcutHelp, 
+  useTabShortcuts, 
+  defaultShortcuts 
+} from './useKeyboardShortcuts'
+export type { KeyboardShortcut } from './useKeyboardShortcuts'
